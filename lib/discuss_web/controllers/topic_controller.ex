0af3715cff041e683e91474cd6259a4f06968ce6
higor-haider <higor.haider@sumup.com>
2022-06-24 20:21:28 -0300

    Adds :delete function

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -50,4 +50,12 @@ defmodule DiscussWeb.TopicController do
 
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
