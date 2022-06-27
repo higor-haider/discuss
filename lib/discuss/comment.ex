334d59ca2c5cf725d2fbbb5361f2b661625b8bd5
higor-haider <higor.haider@sumup.com>
2022-06-27 14:16:45 -0300

    Enhancements to comments feature

--- lib/discuss/comment.ex
+++ lib/discuss/comment.ex
@@ -3,6 +3,8 @@ defmodule Discuss.Comment do
 
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:content]}
+
   schema "comments" do
     field :content, :string
     belongs_to :user, Discuss.User
--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -6,21 +6,27 @@ defmodule DiscussWeb.CommentsChannel do
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
-    topic = Repo.get(Topic, topic_id)
 
-    {:ok, %{}, assign(socket, :topic, topic)}
+    topic =
+      Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(comments: [:user])
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, %{"content" => content}, socket) do
+  def handle_in(_name, %{"content" => content}, socket) do
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset =
       topic
-      |> Ecto.build_assoc(:comments)
+      |> Ecto.build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
       {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
         {:reply, :ok, socket}
 
       {:error, _reason} ->
