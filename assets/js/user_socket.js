2236e80c2b6efdd4bd88266cb0b1bbfe681f652a
higor-haider <higor.haider@sumup.com>
2022-06-27 00:24:30 -0300

    Implements websockets usage

---
+++ assets/js/user_socket.js
@@ -0,0 +1,13 @@
+import {Socket} from "phoenix"
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+
+socket.connect()
+
+const createSocket = (topicId) => {
+let channel = socket.channel(`comments:${topicId}`, {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+}
+
+window.createSocket = createSocket;
---
+++ lib/discuss_web/channels/comments_channel.ex
@@ -0,0 +1,15 @@
+defmodule DiscussWeb.CommentsChannel do
+  use DiscussWeb, :channel
+  alias Discuss.Topic
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
+  end
+
+  def handle_in(name, message, socket) do
+    {:reply, :ok, socket}
+  end
+end
